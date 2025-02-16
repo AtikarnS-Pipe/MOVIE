27e1af769f143549977e1bffb87586da6e492bab
Atikarn Sirinorakul <atikarnn2547@gmail.com>
2025-02-16 16:58:50 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import 'normalize.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
